67b4a5986e751aff5eecdff703f3c8da1ed5c81c
Dan Poxton <d.poxton@proteo.co.uk>
2014-07-09 09:20:16 +0100

    MWF.Mobile: Basic dataservice and tests.

---
+++ MWF.Mobile.Core/Services/DataService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using MWF.Mobile.Core.Models;
+using Cirrious.MvvmCross.Community.Plugins.Sqlite;
+
+
+
+namespace MWF.Mobile.Core.Services
+{
+
+    public class DataService : MWF.Mobile.Core.Services.IDataService
+    {
+
+        #region Private Members
+
+        private ISQLiteConnection _connection;
+        private const string DBNAME = "db.sql";
+
+        #endregion
+
+        #region Construction
+
+        public DataService(ISQLiteConnectionFactory connectionFactory)
+        {
+            _connection = connectionFactory.Create(DBNAME);
+            _connection.CreateTable<Customer>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ISQLiteConnection Connection
+        {
+            get { return _connection; }
+        }
+        
+
+        #endregion
+
+
+    }
+
+}
---
+++ MWF.Mobile.Core/Services/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Cirrious.MvvmCross.Community.Plugins.Sqlite;
+
+namespace MWF.Mobile.Core.Services
+{
+    public interface IDataService
+    {
+        ISQLiteConnection Connection { get; }
+    }
+}

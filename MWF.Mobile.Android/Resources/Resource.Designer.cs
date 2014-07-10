79afa83ba13d781e2581ecc7bdeb0c30e8020568
Joseph Moore <j.moore@proteo.co.uk>
2014-07-10 17:29:04 +0100

    Deleting of triangle Icon.

--- MWF.Mobile.Android/Resources/Resource.Designer.cs
+++ MWF.Mobile.Android/Resources/Resource.Designer.cs
@@ -83,9 +83,6 @@ namespace MWF.Mobile.Android
 			// aapt resource value: 0x7f020005
 			public const int splash = 2130837509;
 			
-			// aapt resource value: 0x7f020006
-			public const int triangleIcon = 2130837510;
-			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();

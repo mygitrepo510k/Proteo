83fed0ccbedd0b57444941050ad154fa798ecbeb
Joseph Moore <j.moore@proteo.co.uk>
2014-07-22 15:37:24 +0100

    Fixed sound problem, update vehicle theme.

--- MWF.Mobile.Android/Resources/Resource.Designer.cs
+++ MWF.Mobile.Android/Resources/Resource.Designer.cs
@@ -362,26 +362,32 @@ namespace MWF.Mobile.Android
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f07000a
-			public const int Mono_Android_Theme_Splash = 2131165194;
+			// aapt resource value: 0x7f07000c
+			public const int Mono_Android_Theme_Splash = 2131165196;
 			
 			// aapt resource value: 0x7f070001
 			public const int Proteo = 2131165185;
 			
-			// aapt resource value: 0x7f070004
-			public const int Proteo_Button = 2131165188;
-			
-			// aapt resource value: 0x7f070005
-			public const int Proteo_Button_Bottom = 2131165189;
-			
 			// aapt resource value: 0x7f070006
-			public const int Proteo_EditText = 2131165190;
+			public const int Proteo_Button = 2131165190;
 			
 			// aapt resource value: 0x7f070007
-			public const int Proteo_EditText_NumberPasscode = 2131165191;
+			public const int Proteo_Button_Bottom = 2131165191;
 			
 			// aapt resource value: 0x7f070008
-			public const int Proteo_ImageView_MWFLogo = 2131165192;
+			public const int Proteo_EditText = 2131165192;
+			
+			// aapt resource value: 0x7f070009
+			public const int Proteo_EditText_NumberPasscode = 2131165193;
+			
+			// aapt resource value: 0x7f07000a
+			public const int Proteo_ImageView_MWFLogo = 2131165194;
+			
+			// aapt resource value: 0x7f070004
+			public const int Proteo_TextView_ItemText = 2131165188;
+			
+			// aapt resource value: 0x7f070005
+			public const int Proteo_TextView_ListHeading = 2131165189;
 			
 			// aapt resource value: 0x7f070002
 			public const int Proteo_TextView_MainHeading = 2131165186;
@@ -392,8 +398,8 @@ namespace MWF.Mobile.Android
 			// aapt resource value: 0x7f070000
 			public const int ProteoNoMargins = 2131165184;
 			
-			// aapt resource value: 0x7f070009
-			public const int Theme_Splash = 2131165193;
+			// aapt resource value: 0x7f07000b
+			public const int Theme_Splash = 2131165195;
 			
 			static Style()
 			{

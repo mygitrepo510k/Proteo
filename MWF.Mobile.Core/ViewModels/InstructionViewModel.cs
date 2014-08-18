25168e67da0fb155c00e9d1db57789df84795c6e
Dan Poxton <d.poxton@proteo.co.uk>
2014-08-18 09:09:12 +0100

    Fixing build issue with instruction view model.

--- MWF.Mobile.Core/ViewModels/InstructionViewModel.cs
+++ MWF.Mobile.Core/ViewModels/InstructionViewModel.cs
@@ -125,6 +125,14 @@ namespace MWF.Mobile.Core.ViewModels
             }
         }
 
+        public ICommand ShowOrderCommand
+        {
+            get
+            {
+                return (_showOrderCommand = _showOrderCommand ?? new MvxCommand<Item>(v => ShowOrder(v)));
+            }
+        }
+
         
 
         #endregion

a43457ae86968d12954e435c6ed5daf45cb6bb66
Joseph Moore <j.moore@proteo.co.uk>
2015-04-28 16:32:46 +0100

    PROT-1041 Added response when you scan a barcode in the list. Added complete button which is locked till all the items are scanned

---
+++ MWF.Mobile.Core/Converters/ScanStateEnumValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using Cirrious.CrossCore.UI;
+using Cirrious.MvvmCross.Plugins.Visibility;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWF.Mobile.Core.Converters
+{
+    public class ScanStateEnumValueConverter : MvxBaseVisibilityValueConverter<Enums.ScanState>
+    {
+        protected override MvxVisibility Convert(Enums.ScanState value, object parameter, CultureInfo culture)
+        {
+            return (value.ToString() == (string)parameter) ? MvxVisibility.Visible : MvxVisibility.Collapsed;
+        }
+    }
+}
---
+++ MWF.Mobile.Core/Enums/ScanState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MWF.Mobile.Core.Enums
+{
+    public enum ScanState
+    {
+        NotScanned = 0,
+        Scanned = 1,
+    }
+}
